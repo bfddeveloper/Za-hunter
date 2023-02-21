03d9e00d8df810c7dfbf79d832b2f670382b75ab
bfddeveloper <dicksonbrody@icloud.com>
2023-02-20 17:05:25 -0700

    Initial Commit

---
+++ Za hunter/Za_hunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_hunterApp.swift
+//  Za hunter
+//
+//  Created by Brody Dickson on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Za_hunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
